11fddb8e1e0a313139784e35df3afac439a7e805
Rocky Bernstein <rocky@gnu.org>
2013-04-13 16:37:06 -0400

    Add the missing ingredient.

---
+++ goplay/goplay.go
@@ -0,0 +1,183 @@
+// Copyright 2013 Rocky Bernstein.
+// Copyright 2010 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"strconv"
+	"text/template"
+)
+
+var (
+	httpListen = flag.String("http", "127.0.0.1:3999", "host:port to listen on")
+	htmlOutput = flag.Bool("html", false, "render program output as HTML")
+)
+
+var (
+	// a source of numbers, for naming temporary files
+	uniq = make(chan int)
+)
+
+func main() {
+	flag.Parse()
+
+	// source of unique numbers
+	go func() {
+		for i := 0; ; i++ {
+			uniq <- i
+		}
+	}()
+
+	http.HandleFunc("/", edit)
+	http.HandleFunc("/compile", Compile)
+	http.Handle("/static/", http.StripPrefix("/static/",
+		http.FileServer(http.Dir("../static"))))
+	fmt.Printf("Runnning Go Play. Attempting to listening on %s\n", *httpListen)
+	log.Fatal(http.ListenAndServe(*httpListen, nil))
+}
+
+var editTemplate = template.Must(template.ParseFiles("edit.html"))
+
+type Snippet struct {
+	Body []byte
+}
+
+type editData struct {
+	Snippet *Snippet
+}
+
+// edit is an HTTP handler that renders the goplay interface.
+// If a filename is supplied in the path component of the URI,
+// its contents will be put in the interface's text area.
+// Otherwise, the default "hello, world" program is displayed.
+func edit(w http.ResponseWriter, req *http.Request) {
+	data, err := ioutil.ReadFile(req.URL.Path[1:])
+	if err != nil {
+		data = []byte(hello)
+	}
+
+	snip := &Snippet{Body: data}
+	editTemplate.Execute(w, &editData{snip})
+}
+
+// Compile is an HTTP handler that reads Go source code from the request,
+// runs the program (returning any errors),
+// and sends the program's output as the HTTP response.
+func Compile(w http.ResponseWriter, req *http.Request) {
+	out, err := compile(req)
+	if err != nil {
+		error_(w, out, err)
+		return
+	}
+
+	// write the output of x as the http response
+	if *htmlOutput {
+		w.Write(out)
+	} else {
+		output.Execute(w, out)
+	}
+}
+
+var (
+	commentRe = regexp.MustCompile(`(?m)^#.*\n`)
+	tmpdir    string
+)
+
+func init() {
+	// find real temporary directory (for rewriting filename in output)
+	var err error
+	tmpdir, err = filepath.EvalSymlinks(os.TempDir())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func compile(req *http.Request) (out []byte, err error) {
+	// x is the base name for .go, .6, executable files
+	x := filepath.Join(tmpdir, "compile"+strconv.Itoa(<-uniq))
+	src := x + ".go"
+	bin := x
+	if runtime.GOOS == "windows" {
+		bin += ".exe"
+	}
+
+	// rewrite filename in error output
+	defer func() {
+		if err != nil {
+			// drop messages from the go tool like '# _/compile0'
+			out = commentRe.ReplaceAll(out, nil)
+		}
+		out = bytes.Replace(out, []byte(src+":"), []byte("main.go:"), -1)
+	}()
+
+	// write body to x.go
+	body := new(bytes.Buffer)
+	if _, err = body.ReadFrom(req.Body); err != nil {
+		return
+	}
+	defer os.Remove(src)
+	if err = ioutil.WriteFile(src, body.Bytes(), 0666); err != nil {
+		return
+	}
+
+	// build x.go, creating x
+	dir, file := filepath.Split(src)
+	out, err = run(dir, "go", "build", "-o", bin, file)
+	defer os.Remove(bin)
+	if err != nil {
+		return
+	}
+
+	// run x
+	return run("", bin)
+}
+
+// error writes compile, link, or runtime errors to the HTTP connection.
+// The JavaScript interface uses the 404 status code to identify the error.
+func error_(w http.ResponseWriter, out []byte, err error) {
+	w.WriteHeader(404)
+	if out != nil {
+		output.Execute(w, out)
+	} else {
+		output.Execute(w, err.Error())
+	}
+}
+
+// run executes the specified command and returns its output and an error.
+func run(dir string, args ...string) ([]byte, error) {
+	var buf bytes.Buffer
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Dir = dir
+	cmd.Stdout = &buf
+	cmd.Stderr = cmd.Stdout
+	err := cmd.Run()
+	return buf.Bytes(), err
+}
+
+// HTML output template snippet
+const outputText = `<pre>{{printf "%s" . |html}}</pre>`
+var output = template.Must(template.New("output").Parse(outputText))
+
+
+// Default program to start out with.
+const hello = `package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello, world")
+}
+`
