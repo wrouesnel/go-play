4831f2b97f5ae38e5bdd9c268a3c4c4c52c0ea29
rocky <rocky@gnu.org>
2013-05-10 12:01:04 -0400

    First hybrid test

--- test/basic-test.js
+++ test/basic-test.js
@@ -1,5 +1,9 @@
 // goplay tests
 
+if (typeof require != "undefined") {
+    var buster = require("buster");
+}
+
 buster.testCase("A module", {
     "states the obvious": function () {
         assert(true);
